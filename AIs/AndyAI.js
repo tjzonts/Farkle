2c2adeac4a4b1c1bd16c5932010ecb0731ae3211
tjzonts <tjzonts@verizon.net>
2018-02-24 11:33:29 -0500

    Logic all done

--- AIs/AndyAI.js
+++ AIs/AndyAI.js
@@ -1,3 +1,18 @@
+/*
+Please do not change the function name or filename, as they both are hardcoded in the game.
+Your namesake function will be called on your turn passing a game object with the following properties:
+	Player object containing
+			name, 
+			score (locked in points from previous turns), 
+			currentTurnPoints (points earning this turn, not locked in yet), 
+			hasQualified (boolean whether you have qualified yet), 
+			holding (dice you currently are holding, may remove this), 
+			round (what round the player has completed)
+	qualificationAmt = 500;
+Please return an object with an array of dice (face value) you plan on holding onto for scoring
+named "diceHolding" and a boolean named "rollAgain" to indicate whether you want to continue or
+end your turn; sample:  return { diceHolding: [1,1,2,2,2], rollAgain: false}
+	*/
 function AndyTurn(){
 	var response = { diceHolding: [1,1], rollAgain: false}
 		return response;	

d53f601c634e63ac3ba3399dc3975bbbfe2da10c
TehGeer <36781627+TehGeer@users.noreply.github.com>
2018-03-01 20:08:09 -0500

    Show hotstreaks and qualifying in UI

--- GameUi/EpicUi.js
+++ GameUi/EpicUi.js
@@ -11,7 +11,11 @@ function beginTurnUpdateUi(currentPlayer) {
 }
 
 function endTurnUpdateUi(currentPlayer) {
-    println(currentPlayer.name + "TextArea", "Points Gained this Round: " + currentPlayer.recentTurn.turnPoints);
+    if (currentPlayer.hasQualified && currentPlayer.score === 0 && !currentPlayer.disqualified && !currentPlayer.bust)
+        println(currentPlayer.name + "TextArea", "Qualified to score this Round!");
+    else
+        println(currentPlayer.name + "TextArea", "Points Gained this Round: " + currentPlayer.recentTurn.turnPoints);
+        
     println(currentPlayer.name + "TextArea", "Total Score: " + currentPlayer.score + "\n");
 }
 
@@ -19,22 +23,27 @@ function gameOverUpdateUi(winnerName) {
     print(winnerName, " - WINNER!");
 }
 
-function displayDice(currentPlayer, currentRoll){
+function displayDice(currentPlayer, currentRoll) {
     var dice = "Roll " + currentRoll.rollSequence + "\t[" + currentRoll.roll.toString() + "] ";
     var heldDice = "Held\t\t[" + currentRoll.hold.toString() + "]";
+    var hotstreak = false;
     var rollPnts = "";
-    if (currentRoll.disqualified)
+    if (currentRoll.disqualified) {
         rollPnts = "CHEATER!";
-    else if (currentRoll.bust)
+    } else if (currentRoll.bust) {
         rollPnts = "BUST!\t";
-    else
+    } else {
         rollPnts = "+" + currentRoll.holdPoints + " pnts";
-
-    var points = rollPnts + "\tRound pnts: " + currentPlayer.recentTurn.turnPoints + "\n";
+        if (currentRoll.roll.length === currentRoll.hold.length)
+            hotstreak = true;
+    }
+    var points = rollPnts + "\tRound pnts: " + currentPlayer.recentTurn.turnPoints + "\n";    
 
     println(currentPlayer.name + "TextArea", dice);
     println(currentPlayer.name + "TextArea", heldDice);
     println(currentPlayer.name + "TextArea", points);
+    if (hotstreak)
+        println(currentPlayer.name + "TextArea", "HOTSTREAK! You're Unstoppable!!!\n");
 }
 
 function println(id, text = "") {
